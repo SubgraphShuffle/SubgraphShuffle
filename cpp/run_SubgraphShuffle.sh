5f7261e6a59c3d927a8a6b1ca6d450f06b182bdb
anonymous97531 <92283162+anonymous97531@users.noreply.github.com>
2022-05-07 21:49:13 +0900

    Add files via upload

--- cpp/run_SubgraphShuffle.sh
+++ cpp/run_SubgraphShuffle.sh
@@ -5,10 +5,10 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 2n
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 3n
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 4n
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 5
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 6-0.1
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 8n
-./SubgraphShuffle ../data/${1}/edges.csv 10000 1-8 -1 20-1 9
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 2n
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 3n
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 4n
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 5
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 6-0.1
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 8n
+./SubgraphShuffle ../data/${1}/edges.csv 20000 1-8 -1 20-1 9
